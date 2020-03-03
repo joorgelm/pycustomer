d3cfdfbcd12b6b892c1386a92e371e81fd948e0a
Jorge Melgarejo <melgarejo.colarte@gmail.com>
2020-03-02 22:39:04 -0400

    cadastro de usuario

--- feedbacks/views.py
+++ feedbacks/views.py
@@ -1,4 +1,8 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
 
 from django.contrib.auth.models import User
 
@@ -20,3 +24,14 @@ class UserViewSet(viewsets.ModelViewSet):
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+
+        user = User.objects.create_user(username=request.data['username'], password=request.data['password'])
+        token = Token.objects.create(user=user)
+        data = {
+            'username': user.username,
+            'token': token.key
+        }
+
+        return Response(data=data, status=status.HTTP_201_CREATED)
