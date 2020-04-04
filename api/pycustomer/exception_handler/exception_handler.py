ca697d5c3227562820d4bc55796e47e2843adad3
Jorge Melgarejo <melgarejo.colarte@gmail.com>
2020-04-04 16:06:12 -0400

    tratando exceção usuario ja cadastrado

--- api/pycustomer/exception_handler/exception_handler.py
+++ api/pycustomer/exception_handler/exception_handler.py
@@ -1,20 +1,17 @@
 from rest_framework.views import exception_handler
-from rest_framework.exceptions import APIException
+from rest_framework import status
+from rest_framework.response import Response
+from django.db.utils import IntegrityError
 
 
 def base_exception_handler(exc, context):
-    # Call REST framework's default exception handler first,
-    # to get the standard error response.
+
+    if isinstance(exc, IntegrityError):
+        return Response(data={'message': 'usuário já cadastrado'}, status=status.HTTP_409_CONFLICT)
+
     response = exception_handler(exc, context)
 
-    # Now add the HTTP status code to the response.
     if response is not None:
-        response.data['status_code'] = 300
+        response.data['status_code'] = response.status_code
 
     return response
-
-
-class CustomAPIException(APIException):
-    status_code = 400
-    default_detail = "Unable to read file."
-    default_code = "unreadable_csv_file"
