2bc959a339ef57b825a7e7d37b5381f8b98d2073
Jorge Melgarejo <melgarejo.colarte@gmail.com>
2020-04-07 17:32:04 -0400

    mensagem de erro para senha errada

--- api/pycustomer/exception_handler/exception_handler.py
+++ api/pycustomer/exception_handler/exception_handler.py
@@ -1,15 +1,17 @@
 from rest_framework.views import exception_handler
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework import exceptions as exp
 from django.db.utils import IntegrityError
 
 
 def base_exception_handler(exc, context):
 
-    # todo: tratar erro de senha errada
+    if isinstance(exc, exp.ValidationError):
+        return Response(data={'message': 'Usuário e/ou senha inválidos'}, status=status.HTTP_401_UNAUTHORIZED)
 
     if isinstance(exc, IntegrityError):
-        return Response(data={'message': 'usuário já cadastrado'}, status=status.HTTP_409_CONFLICT)
+        return Response(data={'message': 'Usuário já cadastrado'}, status=status.HTTP_409_CONFLICT)
 
     response = exception_handler(exc, context)
 
