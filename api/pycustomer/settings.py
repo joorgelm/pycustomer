e778b86e655e63881e708a092c3875e44be1aebb
jorge.melgarejo <jorge.melgarejo@azi.com.br>
2020-04-07 20:10:51 -0400

    WIP postgresql

--- api/pycustomer/settings.py
+++ api/pycustomer/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'pycustomer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('POSTGRES_DB_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
